7d3ad87fa0285ff22d9d6aa6dbbc41e23db5b0a7
binbin <binbin@kooboo.com>
2015-07-15 12:04:26 +0800

    testor 2

--- RedisPresureTest/Program.cs
+++ RedisPresureTest/Program.cs
@@ -14,38 +14,27 @@ namespace RedisPresureTest
         {
 
             var redisManager = new PooledRedisClientManager(0, "127.0.0.1:6379");
-            var tasks = new List<Task>();
 
-            for (int i = 0; i < 50; i++)
-            {
-                var testor = new Testor1();
-                testor.TaskName = "task" + i;
-                var thread = new Thread(new ThreadStart(testor.Test));
-                thread.Start();
-            }
 
             //for (int i = 0; i < 50; i++)
             //{
             //    var testor = new Testor1();
             //    testor.TaskName = "task" + i;
-            //    var task = new Task(() =>
-            //    {
-            //        testor.Test();
-            //    });
-            //    tasks.Add(task);
-            //    task.Start();
+            //    var thread = new Thread(new ThreadStart(testor.Test));
+            //    thread.Start();
             //}
-            //try
-            //{
-            //    Task.WaitAll(tasks.ToArray());
-            //}
-            //catch (AggregateException exception)
-            //{
-            //    foreach (var item in exception.InnerExceptions)
-            //    {
-            //        Console.WriteLine(item.Message);
-            //    }
-            //}
+
+
+            for (var j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 150; i++)
+                {
+                    var testor = new Testor2();
+                    testor.TaskName = "task" + i;
+                    var thread = new Thread(new ThreadStart(testor.Test));
+                    thread.Start();
+                }
+            }
             Console.WriteLine("End");
             Console.Read();
         }
---
+++ RedisPresureTest/Testor2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisPresureTest
+{
+    public class Testor2 : AbstractTestor
+    {
+        public override void Test()
+        {
+            var langauges = new string[] { "nl-nl", "en-us", "de-de" };
+            int i = 0, j = 0;
+            var client = RedisManager.GetCacheClient();
+            var key = string.Format("NewestProducts3:{0}:{1}", i, langauges[j]);
+            var data = client.Get<CachedValue<List<ProductModel>>>(key);
+            Console.WriteLine(string.Format("Read {0} {1}:{2}", TaskName, i, j));
+            if (data == null)
+            {
+                Thread.Sleep(1 * 3000);
+                var cachedValue = new CachedValue<List<ProductModel>>() { Value = GetProducts() };
+
+                client.Set(key, cachedValue);
+                Console.WriteLine(string.Format("Write {0} {1}:{2}", TaskName, i, j));
+
+
+            }
+            client.Remove(key);
+        }
+    }
+}
